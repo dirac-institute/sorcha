e436dd8248816251ea2b96a614916b3596dfbb10
Meg Schwamb <mschwamb>
2021-01-01 16:49:00 +0000

    update doc strings so they show up with sphynx runs
    update doc strings so they show up with sphynx runs

--- modules/PPreadColours.py
+++ modules/PPreadColours.py
@@ -2,26 +2,9 @@
 
 import pandas as pd
 
-"""
-PPreadColours.py
 
-Author: Grigori Fedorets
+#Author: Grigori Fedorets
 
-Description: This task reads in the colours file and puts it into a 
-single pandas dataframe for further use downstream by other tasks.
-
-The format of the colours is:
-
-id   colour1 colour2 etc
-
-
-Mandatory input:      Output from colour file in text file
-
-Output:               pandas dataframe
-
-
-usage: padafr=PPreadColours(clr_datafile)
-"""
 
 
 def PPreadColours(clr_datafile):
@@ -29,8 +12,8 @@ def PPreadColours(clr_datafile):
     PPreadColours.py
 
 
-    Description: This task reads in the colours file and puts it into a
-    single pandas dataframe for further use downstream by other tasks.
+    Description: This task reads in the colours file and puts it into a single pandas dataframe for further use downstream by other tasks.
+
     
     The format of the colours is:
     
