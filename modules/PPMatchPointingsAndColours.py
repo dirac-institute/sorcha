4419c40acd686939010cbd67fec27cfa6092e2f9
Meg Schwamb <mschwamb>
2021-01-01 22:38:38 +0000

    updating docstrings
    updating docstrings to work with sphynx

--- modules/PPMatchPointingsAndColours.py
+++ modules/PPMatchPointingsAndColours.py
@@ -5,31 +5,32 @@ import pandas as pd
 import numpy as np
 import random
 
-"""
-PPMatchPointingsAndColours.py
+# Author: Grigori Fedorets
 
-Author: Grigori Fedorets
-
-Description: This task takes two pandas dataframes: one is output that includes all 
-relevant colours, the other is a two-column database that includes all the fieldId:s
-and the filters at the given pointing.
-
-The task cross-matches the two databases, matches the pre-calculated colour in each 
-pointing with the relevant filter. Then it leaves only the relevant colour and the
-filter, and erases all the obsolete colours.
-
-Mandatory input:      Output from objectsInField (oif) with additional relevant colour
-                      columns (pandas dataframes)
-                      pointing and filter dataframe (pandas dataframe)
-
-Output:               pandas dataframe
-
-
-usage: padafr=PPMatchPointingsAndColours(padain,pointfildb)
-"""
 
 
 def PPMatchPointingsAndColours(padain,pointfildb):
+    """
+
+    Description: This task takes two pandas dataframes: one is output that includes all
+    relevant colours, the other is a two-column database that includes all the fieldId:s
+    and the filters at the given pointing.
+    
+    The task cross-matches the two databases, matches the pre-calculated colour in each
+    pointing with the relevant filter. Then it leaves only the relevant colour and the
+    filter, and erases all the obsolete colours.
+    
+    Mandatory input:      Output from objectsInField (oif) with additional relevant colour
+                          columns (pandas dataframes)
+                          pointing and filter dataframe (pandas dataframe)
+        
+    Output:               pandas dataframe
+    
+    
+    usage: padafr=PPMatchPointingsAndColours(padain,pointfildb)
+    """
+
+
     
     possible_colours=pd.Series(['u','g','r','i','z','y'])
     
