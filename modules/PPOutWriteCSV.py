e6ff5e075202c98e6ab2eb2ac6aab45bb844612c
fedorets <grigorifedorets@hotmail.com>
2021-01-06 18:38:41 +0200

    Commit 06.01.2021 / GF
    Adding csv writing to output

---
+++ modules/PPOutWriteCSV.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+import pandas as pd
+
+
+
+def PPOutWriteCSV(padain, outf):
+    """
+    PPOutWriteCSV.py
+
+    Author: Grigori Fedorets
+
+    Description: This task reads in the pandas database, and writes out a CSV file by a name given by the user. 
+
+
+    Mandatory input:      name of database, name of output file
+
+    Output:               CSV file
+
+
+    usage: padafr=PPOutWriteCSV(padain,outf)
+    """
+
+
+    padain=padain.to_csv(path_or_buf=outf, index=False)
+    return
