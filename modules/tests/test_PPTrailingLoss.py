bd5cedadc7bb25cabdf239b567bc7c324fb70ce3
samncorn <samncorn@uw.edu>
2021-08-06 16:58:43 -0700

    Fixed Trailing losses test so that it actually tests something.

--- modules/tests/test_PPTrailingLoss.py
+++ modules/tests/test_PPTrailingLoss.py
@@ -3,17 +3,28 @@ import numpy as np
 import pandas as pd
 
 from ..PPMatchFieldConditions import PPMatchFieldConditions
-from ..PPTrailingLoss import PPTrailingLoss
+from ..PPTrailingLoss import calcTrailingLoss, PPTrailingLoss
+
+def test_calcTrailingLoss():
+    assert 0.45347433560262890 == PPTrailingLoss.calcTrailingLoss(1.0, 1.0, 1.0)
+    return
 
 def test_PPTrailingLoss():
-    #Test calcTrailingLoss function
-
-    test_oif=pd.read_csv('./data/test/oiftestoutput', delim_whitespace=True)
-    seeing,_=PPMatchFieldConditions('./data/baseline_10yrs_10klines.db')
-    seeing.rename(columns={'seeing': 'seeingFwhmGeom'}, inplace=True)
-
-    test_out=PPTrailingLoss(test_oif, seeing, field_id_name_survey='FieldID')
-    print(test_out)
-
-    #assert test_out['trailing loss'][0]==
-    return
+    testoifdf = pd.DataFrame({"FieldID" : np.arange(0, 10),
+                          "AstRARate(deg/day)" :  [0.2302361,  0.59039849, 0.51966227,
+                           0.35292237, 0.34375913, 0.29833637, 0.28306961, 0.16266355,
+                           0.23515252, 0.49184532],
+                          "AstDecRate(deg/day)":  [0.07060338, 0.63168302, 0.90364059,
+                           0.69532128, 0.39521156, 0.43196356, 0.47147488, 0.72890365,
+                           0.38174909, 0.03332251],
+                          "AstDec(deg)":         [0.45697193, 0.39612113, 0.91493552,
+                           0.21664349, 0.2001591, 0.8722614,  0.08539993, 0.07318935,
+                           0.74395827, 0.47139718]
+    })
+    testsurveydf = pd.DataFrame({
+                    "observationId": np.arange(0, 10),
+                    "seeingFwhmGeom": [0.96801413, 0.35969965, 0.33634724, 0.43889922,
+                                       0.97841924, 0.54296975, 0.99666377, 0.95417028,
+                                       0.41872633, 0.85249683]
+    })
+    assert 0.25893924959480374 == PPTrailingLoss.PPTrailingLoss(testoifdf, testsurveydf)[5]
