5411a8cdc4f0384f1bbcc76e5d4c1d38251717f7
Meg Schwamb <megan.schwamb@gmail.com>
2021-08-14 22:58:56 +0100

    Update makeConfigOIF.py
    made two of the arguments mandatory, fixed bug in config filename if less than the desired number of objects per file, do some reasonable error checks, outputs default values with the --help command line argument

--- makeConfigOIF.py
+++ makeConfigOIF.py
@@ -57,12 +57,11 @@ def makeConfig():
 
     m = len(startingOrbits)
     for i in range(m):
-        if i == m and remainder!= 0:
+        if (i == (m-1)) and  (remainder!= 0):
             nOrbits = remainder
         else:
             nOrbits = nOrbitsPerFile
-
-
+  
         config.read_dict({
             'CONF': {
                 'Cache dir':       args.cache+"/" + orbitsName + "/" + str(startingOrbits[i]) + '-' + str(nOrbits),       # Directory where to place generated temporary files
@@ -102,36 +101,30 @@ def makeConfig():
 
 if (__name__=='__main__'):
 
-    parser=argparse.ArgumentParser()
-    parser.add_argument("-o", help="orbits file", type=str)
-    parser.add_argument("-no", help="number of orbits per config file", type=int, default=-1)
-    parser.add_argument("-ndays", help="number of days in survey to run, -1 runs entire survey", type=int, default=-1)
-    parser.add_argument("-day1", help="first day in survey to run", type=int, default=1)
-    parser.add_argument("-pointing", help="path to pointing database", type=str)
-    parser.add_argument("-prefix", help="config file name prefix", type=str, default='')
-    parser.add_argument("-camerafov", help='path and file name of the camera fov', type=str, default='instrument_polygon.dat')
-    parser.add_argument("-inputformat", help='input format (CSV or whitespace)', type=str, default='whitespace')
-    parser.add_argument("-cache", help='base cache directory name.', type=str, default='_cache')
-    parser.add_argument("-mpcfile", help='name of the file containing the MPC observatory codes', type=str, default='obslist.dat')
+    parser=argparse.ArgumentParser(description='creating config file(s) for Objects in Field')
+    parser.add_argument("o", help="orbits file", type=str)
+    parser.add_argument("pointing", help="pointing database", type=str)
+    parser.add_argument("-no", help="number of orbits per config file, -1 runs all the orbits in one config file. Default value = 300", type=int, default=300)
+    parser.add_argument("-ndays", help="number of days in survey to run, -1 runs entire survey. Default value = -1", type=int, default=-1)
+    parser.add_argument("-day1", help="first day in survey to run. Default value = 1", type=int, default=1)
+    parser.add_argument("-prefix", help="config file name prefix, Default value is an empty string", type=str, default='')
+    parser.add_argument("-camerafov", help='path and file name of the camera fov. Default value = instrument_polygon.dat', type=str, default='instrument_polygon.dat')
+    parser.add_argument("-inputformat", help='input format (CSV or whitespace). Default value = whitespace', type=str, default='whitespace')
+    parser.add_argument("-cache", help='base cache directory name. Default value = _cache', type=str, default='_cache')
+    parser.add_argument("-mpcfile", help='name of the file containing the MPC observatory codes. Default value = obslist.dat', type=str, default='obslist.dat')
 
 
     args = parser.parse_args()
-    if (args.o == None):
-       sys.exit('ERROR: orbit file not specified. Try --help to see the command line options')
-    else:
-       if (os.path.exists(args.o)) == False:
-         sys.exit('ERROR: orbits file does not exist.')
-
-    if (args.pointing == None):
-       sys.exit('ERROR: pointing database file not specified. Try --help to see the command line options')
-    else:
-       if (os.path.exists(args.pointing)) == False:
-         sys.exit('ERROR: pointing databaae file does not exist.')
-
-    if (args.o == None):
-       sys.exit('ERROR: orbit file not specified. Try --help to see the command line options')
 
     if (args.inputformat != 'whitespace') and (args.inputformat != 'CSV'):
        sys.exit('ERROR: Invalid option for input format of the orbits file.  Try --help to see the command line options')
 
+    if (args.no < -1):
+       sys.exit('ERROR: Invalid option for number of orbits per config file.  Try --help to see the command line options')
+
+    if (args.day1 <1):
+       sys.exit('ERROR: Invalid option for first day in survey to run.  Try --help to see the command line options')
+
+    if (args.ndays ==0) or (args.ndays < -1):
+       sys.exit('ERROR: Invalid option for number of days in survey to run.  Try --help to see the command line options')
     makeConfig()
