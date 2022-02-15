af6e34ff79a1edebed5c62ae85aed6d4d4c1fa5a
Siegfried Eggl <eggl@jupiter.ae.illinois.edu>
2022-02-15 11:54:34 -0600

    Added license and Rubin/LSST preables where necessary.

--- modules/tests/test_PPAddUncertaintiy.py
+++ modules/tests/test_PPAddUncertaintiy.py
@@ -1,3 +1,22 @@
+# Developed for the Vera C. Rubin Observatory/LSST Data Management System.
+# This product includes software developed by the
+# Vera C. Rubin Observatory/LSST Project (https://www.lsst.org).
+#
+# Copyright 2020 University of Washington
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import pytest
 import numpy as np
 import pandas as pd
