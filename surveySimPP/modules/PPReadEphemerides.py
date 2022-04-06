7176fd1f3eacd1ebf8a91a9554a00b56cbc14177
fedorets <grigorifedorets@hotmail.com>
2022-04-06 12:35:38 +0300

    Update 06.04.2022 / GF
    Fix for case insensitivity of PPReadEphemerides

--- surveySimPP/modules/PPReadEphemerides.py
+++ surveySimPP/modules/PPReadEphemerides.py
@@ -41,7 +41,7 @@ def PPReadEphemerides(eph_output, ephemerides_type, inputformat):
    """
    
    ephemerides_type=ephemerides_type.casefold()
-   print(ephemerides_type)
+
    
    if (ephemerides_type=='oif'):
          padafr=PPReadOif.PPReadOif(eph_output, inputformat)
