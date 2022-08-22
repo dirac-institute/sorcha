1d29e0381f1e234d9159be874bd6e706003173ee
Steph Merritt <s.merritt@qub.ac.uk>
2022-08-22 15:09:39 +0100

    Fixing tests.

--- surveySimPP/modules/tests/test_PPReadOrbitFile.py
+++ surveySimPP/modules/tests/test_PPReadOrbitFile.py
@@ -5,7 +5,7 @@ from surveySimPP.tests.data import get_test_filepath
 
 def test_PPReadOrbitFile():
     from surveySimPP.modules.PPReadOrbitFile import PPReadOrbitFile
-    rescol = 9
+    rescol = 8
 
     padafr = PPReadOrbitFile(get_test_filepath('testorb.des'), 0, 14, "whitespace")
     val = len(padafr.columns)
