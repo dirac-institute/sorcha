5401e2da86f1c040ecb5d672201923ff7131f679
Steph Merritt <s.merritt@qub.ac.uk>
2023-03-14 17:44:51 +0000

    Another broken test on Windows.

--- surveySimPP/modules/tests/test_PPGetLogger.py
+++ surveySimPP/modules/tests/test_PPGetLogger.py
@@ -2,32 +2,15 @@ import glob
 import os
 import pytest
 
-from surveySimPP.tests.data import get_test_filepath
 
-
-@pytest.fixture
-def setup_and_teardown_for_PPGetLogger():
-
-    yield
-
-    test_path = os.path.dirname(get_test_filepath('test_input_fullobs.csv'))
-
-    errlog = glob.glob(os.path.join(test_path, '*-postprocessing.err'))
-    datalog = glob.glob(os.path.join(test_path, '*-postprocessing.log'))
-
-    os.remove(errlog[0])
-    os.remove(datalog[0])
-
-
-def test_PPGetLogger(setup_and_teardown_for_PPGetLogger):
+def test_PPGetLogger(tmp_path):
 
     from surveySimPP.modules.PPGetLogger import PPGetLogger
-    test_path = os.path.dirname(get_test_filepath('test_input_fullobs.csv'))
 
-    PPGetLogger(test_path)
+    PPGetLogger(tmp_path)
 
-    errlog = glob.glob(os.path.join(test_path, '*-postprocessing.err'))
-    datalog = glob.glob(os.path.join(test_path, '*-postprocessing.log'))
+    errlog = glob.glob(os.path.join(tmp_path, '*-postprocessing.err'))
+    datalog = glob.glob(os.path.join(tmp_path, '*-postprocessing.log'))
 
     assert os.path.exists(errlog[0])
     assert os.path.exists(datalog[0])
