a1e6589be3327b84f836946f484417ff808d27dd
drewoldag <47493171+drewoldag@users.noreply.github.com>
2023-06-21 16:18:52 -0700

    Adding a manually triggered action to run the benchmarks.

--- surveySimPP/modules/benchmarks/bench_ApplyColourOffsets.py
+++ surveySimPP/modules/benchmarks/bench_ApplyColourOffsets.py
@@ -8,102 +8,109 @@ from memory_profiler import memory_usage
 
 from surveySimPP.modules.PPApplyColourOffsets import PPApplyColourOffsets
 
-def setup_test_obs(num_copies=1):
-    objects = np.array(['obj1', 'obj1', 'obj1', 'obj1', 'obj2', 'obj2', 'obj2', 'obj2']*num_copies)
 
-    optfilter = np.array(['r', 'g', 'i', 'z', 'r', 'g', 'z', 'i']*num_copies)
+class TestBenchApplyColourOffsets:
+    def setup_test_obs(self, num_copies=1):
+        objects = np.array(['obj1', 'obj1', 'obj1', 'obj1', 'obj2', 'obj2', 'obj2', 'obj2']*num_copies)
 
-    ur = np.array([0.1, 0.1, 0.1, 0.1, 0.2, 0.2, 0.2, 0.2]*num_copies)
-    gr = np.array([0.3, 0.3, 0.3, 0.3, 0.4, 0.4, 0.4, 0.4]*num_copies)
-    ir = np.array([0.5, 0.5, 0.5, 0.5, 0.6, 0.6, 0.6, 0.6]*num_copies)
-    zr = np.array([0.7, 0.7, 0.7, 0.7, 0.8, 0.8, 0.8, 0.8]*num_copies)
+        optfilter = np.array(['r', 'g', 'i', 'z', 'r', 'g', 'z', 'i']*num_copies)
 
-    H = np.array([10.0, 10.0, 10.0, 10.0, 12.0, 12.0, 12.0, 12.0]*num_copies)
-    G = np.array([0.15, 0.15, 0.15, 0.15, 0.12, 0.12, 0.12, 0.12]*num_copies)
+        ur = np.array([0.1, 0.1, 0.1, 0.1, 0.2, 0.2, 0.2, 0.2]*num_copies)
+        gr = np.array([0.3, 0.3, 0.3, 0.3, 0.4, 0.4, 0.4, 0.4]*num_copies)
+        ir = np.array([0.5, 0.5, 0.5, 0.5, 0.6, 0.6, 0.6, 0.6]*num_copies)
+        zr = np.array([0.7, 0.7, 0.7, 0.7, 0.8, 0.8, 0.8, 0.8]*num_copies)
 
-    return pd.DataFrame({'ObjID': objects, 'optFilter': optfilter,
-                                    'u-r': ur, 'g-r': gr, 'i-r': ir, 'z-r': zr,
-                                    'H_r': H, 'GS': G})
+        H = np.array([10.0, 10.0, 10.0, 10.0, 12.0, 12.0, 12.0, 12.0]*num_copies)
+        G = np.array([0.15, 0.15, 0.15, 0.15, 0.12, 0.12, 0.12, 0.12]*num_copies)
 
-def setup_other_colors():
-    return ['u-r', 'g-r', 'i-r', 'z-r']
+        return pd.DataFrame({'ObjID': objects, 'optFilter': optfilter,
+                                        'u-r': ur, 'g-r': gr, 'i-r': ir, 'z-r': zr,
+                                        'H_r': H, 'GS': G})
 
-def setup_obs_filters():
-    return ['r', 'u', 'g', 'i', 'z']
+    def setup_other_colors(self):
+        return ['u-r', 'g-r', 'i-r', 'z-r']
 
-statement = 'PPApplyColourOffsets(test_obs.copy(), "HG", othercolours, observing_filters, "r")'
+    def setup_obs_filters(self):
+        return ['r', 'u', 'g', 'i', 'z']
 
-def create_setup_str(num_copies=1):
-    return f'''
+    def create_runtime_statement(self):
+        return 'PPApplyColourOffsets(test_obs.copy(), "HG", othercolours, observing_filters, "r")'
+
+    def create_setup_str(self, num_copies=1):
+        return f'''
 test_obs = setup_test_obs({num_copies})
 othercolours = setup_other_colors()
 observing_filters = setup_obs_filters()
-'''
+    '''
 
-'''Runtime benchmarking'''
+    '''Runtime benchmarking'''
 
-benchmarks = []
+    def test_bench_runtime(self):
 
-for n in [1_000, 10_000, 100_000]:
+        # benchmarks = []
 
-    t = timeit.Timer(
-        stmt=statement,
-        setup=create_setup_str(n),
-        globals={
-            'PPApplyColourOffsets': PPApplyColourOffsets,
-            'setup_other_colors': setup_other_colors,
-            'setup_test_obs': setup_test_obs,
-            'setup_obs_filters': setup_obs_filters,
-            },
-        )
+        for n in [1_000, 10_000, 100_000]:
 
-    output = t.repeat(repeat=10, number=1)
+            t = timeit.Timer(
+                stmt=self.create_runtime_statement(),
+                setup=self.create_setup_str(n),
+                globals={
+                    'PPApplyColourOffsets': PPApplyColourOffsets,
+                    'setup_other_colors': self.setup_other_colors,
+                    'setup_test_obs': self.setup_test_obs,
+                    'setup_obs_filters': self.setup_obs_filters,
+                    },
+                )
 
-    print("repetitions:", len(output), "avg:", np.mean(output), "std:", np.std(output))
+            output = t.repeat(repeat=10, number=1)
 
-    bench_dict = {
-        'timestamp': str(datetime.datetime.now()),
-        'name': f'ApplyColourOffsets_{n}',
-        'repetitions': len(output),
-        'avg_run_time_of_repetitions': np.mean(output),
-        'std_of_repetitions': np.std(output),
-        'all_run_times': output
-    }
+            print("repetitions:", len(output), "avg:", np.mean(output), "std:", np.std(output))
 
-    benchmarks.append(bench_dict)
+            # bench_dict = {
+            #     'timestamp': str(datetime.datetime.now()),
+            #     'name': f'ApplyColourOffsets_{n}',
+            #     'repetitions': len(output),
+            #     'avg_run_time_of_repetitions': np.mean(output),
+            #     'std_of_repetitions': np.std(output),
+            #     'all_run_times': output
+            # }
 
-'''Memory usage benchmarking'''
+            # benchmarks.append(bench_dict)
 
-mem_usage = []
+    '''Memory usage benchmarking'''
 
-for i in range(10):
-    new_test_obs = setup_test_obs(1_000)
-    other_colours = setup_other_colors()
-    obs_filters = setup_obs_filters()
-    max_usage = memory_usage(
-        (PPApplyColourOffsets, (new_test_obs.copy(), "HG", other_colours, obs_filters, "r"), {}),
-        max_usage=True,
-        interval=0.1, max_iterations=1)
+    def test_bench_memory(self):
 
-    mem_usage.append(max_usage)
+        mem_usage = []
 
-bench_dict = {
-        'timestamp': str(datetime.datetime.now()),
-        'name': f'ApplyColourOffsets_{n}',
-        'repetitions': len(mem_usage),
-        'avg_max_memory_usage': np.mean(mem_usage),
-        'std_of_repetitions': np.std(mem_usage),
-        'all_max_memory_measurements': mem_usage
-    }
+        for i in range(10):
+            new_test_obs = self.setup_test_obs(1_000)
+            other_colours = self.setup_other_colors()
+            obs_filters = self.setup_obs_filters()
+            max_usage = memory_usage(
+                (PPApplyColourOffsets, (new_test_obs.copy(), "HG", other_colours, obs_filters, "r"), {}),
+                max_usage=True,
+                interval=0.1, max_iterations=1)
 
-benchmarks.append(bench_dict)
+            mem_usage.append(max_usage)
 
-print("repetitions:", len(mem_usage), "avg:", np.mean(mem_usage), "std:", np.std(mem_usage))
+        # bench_dict = {
+        #         'timestamp': str(datetime.datetime.now()),
+        #         'name': f'ApplyColourOffsets_{n}',
+        #         'repetitions': len(mem_usage),
+        #         'avg_max_memory_usage': np.mean(mem_usage),
+        #         'std_of_repetitions': np.std(mem_usage),
+        #         'all_max_memory_measurements': mem_usage
+        #     }
+
+        # benchmarks.append(bench_dict)
+
+        print("repetitions:", len(mem_usage), "avg:", np.mean(mem_usage), "std:", np.std(mem_usage))
 
 
-output_dict = {
-    'benchmarks': benchmarks
-}
+        # output_dict = {
+        #     'benchmarks': benchmarks
+        # }
 
-with open('bench_ApplyColourOffset.json', 'w') as output_file:
-    json.dump(output_dict, output_file)
+        # with open('bench_ApplyColourOffset.json', 'w') as output_file:
+        #     json.dump(output_dict, output_file)
