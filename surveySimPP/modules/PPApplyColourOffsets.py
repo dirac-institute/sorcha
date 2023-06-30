d546eeaf6c2cab1422b3bdd0c14b6117257361f9
drewoldag <47493171+drewoldag@users.noreply.github.com>
2023-06-30 14:23:51 -0700

    Run benchmarks on demand only, remove old code that was commented out.

--- surveySimPP/modules/PPApplyColourOffsets.py
+++ surveySimPP/modules/PPApplyColourOffsets.py
@@ -37,10 +37,6 @@ def PPApplyColourOffsets(observations, function, othercolours, observing_filters
 
     # first apply the H offset for every observation
     try:
-        # baseline implementation
-        # observations[H_col] = observations.apply(lambda row: row[H_col] + row[row["optFilter"] + "-" + mainfilter], axis=1)
-
-        # vectorized implementation
         unique_opt_filters = observations["optFilter"].unique()
         for filter in unique_opt_filters:
             mask = observations["optFilter"] == filter
