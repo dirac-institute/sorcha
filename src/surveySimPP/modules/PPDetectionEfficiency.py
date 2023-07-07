b921b3fcf02df8000508495013d6a3e5aecc7e77
drewoldag <47493171+drewoldag@users.noreply.github.com>
2023-07-07 14:47:59 -0700

    Vectorize detection efficiency check.

--- src/surveySimPP/modules/PPDetectionEfficiency.py
+++ src/surveySimPP/modules/PPDetectionEfficiency.py
@@ -25,7 +25,7 @@ def PPDetectionEfficiency(padain, threshold, rng):
 
     pplogger = logging.getLogger(__name__)
 
-    padain = padain.reset_index(drop=True)
+    padain.reset_index(drop=True, inplace=True)
 
     if threshold > 1.0 or threshold < 0.0:
         pplogger.error("ERROR: PPDetectionEfficiency: threshold out of bounds.")
@@ -35,9 +35,4 @@ def PPDetectionEfficiency(padain, threshold, rng):
 
     uniform_distr = rng.random(num_obs)
 
-    padain_drop = padain.drop(padain[uniform_distr > threshold].index)
-
-    # After dropping some lines, the indices should be updated.
-    padain_drop = padain_drop.reset_index(drop=True)
-
-    return padain_drop
+    return padain[uniform_distr <= threshold]
