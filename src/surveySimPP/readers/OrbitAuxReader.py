3c17987ac07cdebd337c25fb4979416fd44b9a8f
Jeremy Kubica <104161096+jeremykubica@users.noreply.github.com>
2023-07-11 08:55:45 -0400

    Add a subclass for reading orbit aux data

---
+++ src/surveySimPP/readers/OrbitAuxReader.py
@@ -0,0 +1,53 @@
+import pandas as pd
+import logging
+import sys
+
+from surveySimPP.readers.CSVReader import CSVDataReader
+
+
+class OrbitAuxReader(CSVDataReader):
+    """A class to read in the auxiliary orbit data files."""
+
+    def __init__(self, filename, sep="csv", header=-1, *args, **kwargs):
+        """A class for reading the object data from a CSV file.
+
+        Parameters:
+        -----------
+        filename (string): location/name of the data file.
+
+        sep (string, optional): format of input file ("whitespace"/"comma"/"csv").
+
+        header (int): The row number of the header. If not provided, does an automatic search.
+        """
+        super().__init__(filename, sep, header, *args, **kwargs)
+
+    def process_and_validate_input_table(self, input_table, **kwargs):
+        """Perform any input-specific processing and validation on the input table.
+        Modifies the input dataframe in place.
+
+        Parameters:
+        -----------
+        input_table (Pandas dataframe): A loaded table.
+
+        Returns:
+        -----------
+        res_df (Pandas dataframe): Returns the input dataframe modified in-place.
+        """
+        pplogger = logging.getLogger(__name__)
+
+        # Check for an H column (which should not be in the orbit file).
+        if "H" in input_table.columns:
+            pplogger.error(
+                "ERROR: PPReadOrbitFile: H column present in orbits data file. H must be included in physical parameters file only."
+            )
+            sys.exit(
+                "ERROR: PPReadOrbitFile: H column present in orbits data file. H must be included in physical parameters file only."
+            )
+
+        # rename i to incl to avoid confusion with the colour i
+        input_table = input_table.rename(columns={"i": "incl"})
+        input_table = input_table.drop(
+            ["INDEX", "N_PAR", "MOID", "COMPCODE", "FORMAT"], axis=1, errors="ignore"
+        )
+
+        return input_table
---
+++ src/surveySimPP/readers/tests/test_OrbitAuxReader.py
@@ -0,0 +1,56 @@
+import numpy as np
+import pytest
+from numpy.testing import assert_equal
+from pandas.testing import assert_frame_equal
+
+from surveySimPP.readers.OrbitAuxReader import OrbitAuxReader
+from surveySimPP.tests.data import get_test_filepath
+
+
+def test_OrbitAuxReader():
+    txt_reader = OrbitAuxReader(get_test_filepath("testorb.des"), "whitespace")
+    orbit_txt = txt_reader.read_rows()
+    assert_equal(len(orbit_txt), 5)
+
+    csv_reader = OrbitAuxReader(get_test_filepath("testorb.csv"), "csv")
+    orbit_csv = csv_reader.read_rows()
+    assert_equal(len(orbit_csv), 5)
+
+    # Check we get the same results from both formats.
+    assert_frame_equal(orbit_csv, orbit_txt)
+
+    # Check that we modify the columns (i -> incl, etc.)
+    expected_columns = np.array(["ObjID", "q", "e", "incl", "node", "argperi", "t_p", "t_0"], dtype=object)
+    assert_equal(expected_columns, orbit_txt.columns.values)
+
+    # Check that we read the correct valude, including dropped columns.
+    expected_first_row = np.array(
+        [
+            "S00000t",
+            0.952105479028,
+            0.504888475701,
+            4.899098347472,
+            148.881068605772,
+            39.949789586436,
+            54486.32292808,
+            54466.0,
+        ],
+        dtype=object,
+    )
+    assert_equal(expected_first_row, orbit_txt.iloc[0].values)
+
+    # Unexpected H column.
+    with pytest.raises(SystemExit) as e1:
+        reader = OrbitAuxReader(get_test_filepath("PPReadOrbitFile_bad.txt"), "whitespace")
+        _ = reader.read_rows(0, 14)
+    assert e1.type == SystemExit
+    assert (
+        e1.value.code
+        == "ERROR: PPReadOrbitFile: H column present in orbits data file. H must be included in physical parameters file only."
+    )
+
+    # Incorrect format.
+    with pytest.raises(SystemExit) as e2:
+        reader = OrbitAuxReader(get_test_filepath("testorb.csv"), "whitespace")
+        _ = reader.read_rows(0, 14)
+    assert e2.type == SystemExit
