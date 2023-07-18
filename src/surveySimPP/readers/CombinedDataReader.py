ed307cf06ae09804271afed8a2d02e4f48ab1d0e
Jeremy Kubica <104161096+jeremykubica@users.noreply.github.com>
2023-07-18 13:54:57 -0400

    First version of tests

--- src/surveySimPP/readers/CombinedDataReader.py
+++ src/surveySimPP/readers/CombinedDataReader.py
@@ -4,18 +4,16 @@ to read individual input files and combining the data into a single table.
 
 The CombinedDataReader object reads the data in blocks to limit memory usage.
 For each blocks, it uses two stages:
-1) It reads a range of individual rows from the ``primary_reader``. This reader
-   can be any of the individual data files as long as reading it in a row-wise
-   fashion produces valid results. It uses the data from the primary_reader to
-   assemble a list of object IDs that it will read in stage 2.
-2) For each of the other readers (``object_readers``) load in all the rows
+1) It reads a range of individual rows from the ``primary_reader``. By default this
+   reader is the first auxiliary data reader, but can be set to the ephemeris reader.
+   This reader is used to extract a list of object IDs for this block.
+2) For each of the readers (ephemeris and auxiliary data) load in all the rows
    corresponding to the object IDs extracted in stage 1.
 
 For example, if the ephemeris file is used as the primary reader, the algorithm
-will load data in blocks of the ephemeris rows and load the auxiliary data
-for just the object IDs on those rows. Alternatively if the orbit file
-is used, the algorithm will load data in blocks of object ids and load *all*
-ephemeris for those objects.
+will load data in blocks of the ephemeris rows and join in the auxiliary data
+for just the object IDs on those rows. It is not guaranteed to include all
+rows for the current objects.
 """
 import logging
 import pandas as pd
@@ -24,47 +22,38 @@ import sys
 
 class CombinedDataReader:
     def __init__(self, **kwargs):
-        self.primary_reader = None
-        self.object_readers = []
+        self.ephem_reader = None
+        self.aux_data_readers = []
         self.block_start = 0
 
-    def add_primary_reader(self, new_reader):
-        """Add a new reader that corresponds to an input data type..
+    def add_ephem_reader(self, new_reader):
+        """Add a new reader for ephemeris data.
 
         Parameters
         ----------
         new_reader (ObjectDataReader): The reader for a specific input file.
         """
         pplogger = logging.getLogger(__name__)
-        if self.primary_reader is not None:
-            pplogger.error("ERROR: Primary reader already set.")
-            sys.exit("ERROR: Primary reader already set.")
+        if self.ephem_reader is not None:
+            pplogger.error("ERROR: Ephemeris reader already set.")
+            sys.exit("ERROR: Ephemeris reader already set.")
+        self.ephem_reader = new_reader
 
-        self.primary_reader = new_reader
-
-    def add_reader(self, new_reader):
-        """Add a new object reader that corresponds to an input data type..
+    def add_aux_data_reader(self, new_reader):
+        """Add a new object reader that corresponds to an auxiliary input data type..
 
         Parameters
         ----------
         new_reader (ObjectDataReader): The reader for a specific input file.
         """
-        self.object_readers.append(new_reader)
+        self.aux_data_readers.append(new_reader)
 
-    def reset_block_start(self, new_start=0):
+    def reset_block_start(self):
         """Reset the starting row for reading blocks of data.
-
-        Parameters
-        ----------
-        new_reader (ObjectDataReader): The reader for a specific input file.
         """
-        pplogger = logging.getLogger(__name__)
-        if new_start < 0:
-            pplogger.error(f"ERROR: Invalid block start ({new_start}).")
-            sys.exit(f"ERROR: Invalid block start ({new_start}).")
         self.block_start = new_start
 
-    def read_block(self, block_size=None, verbose=False, **kwargs):
+    def read_block(self, block_size=None, ephem_primary=False, verbose=False, **kwargs):
         """Reads in a set number of rows from the input, performs
         post-processing and validation, and returns a data frame.
 
@@ -73,6 +62,9 @@ class CombinedDataReader:
         block_size (int, optional): the number of rows to read in.
             Use block_size=None to read in all available data.
             [Default = None]
+
+        ephem_primary (bool, optional): Use the ephemeris reader as the primary
+            reader. Otherwise uses the first auxiliary data reader.
 
         verbose (bool, optional): use verbose logging.
 
@@ -84,26 +76,48 @@ class CombinedDataReader:
         pplogger = logging.getLogger(__name__)
         verboselog = pplogger.info if verbose else lambda *a, **k: None
 
-        if self.primary_reader is None:
-            pplogger.error("ERROR: No primary reader provided.")
-            sys.exit("ERROR: No primary reader provided.")
+        if self.ephem_reader is None:
+            pplogger.error("ERROR: No ephemeris reader provided.")
+            sys.exit("ERROR: No ephemeris reader provided.")
+        if len(self.aux_data_readers) == 0:
+            pplogger.error("ERROR: No auxiliary readers provided.")
+            sys.exit("ERROR: No auxiliary readers provided.")
+                
+        # Load object IDs from the primary table.
+        if ephem_primary:
+            verboselog(f"Loading object IDs from: {self.ephem_reader.get_reader_info()}")
+            ephem_df = self.ephem_reader.read_rows(self.block_start, block_size)
+            self.block_start += len(ephem_df)
 
-        # Load data from the primary table.
-        verboselog(f"Reading primary input file: {self.primary_reader.get_reader_info()}")
-        primary_df = self.primary_reader.read_rows(self.block_start, block_size)
-        objid_list = primary_df["ObjID"].unique().tolist()
+            obj_ids = ephem_df["ObjID"].unique().tolist()
+        else:
+            verboselog(f"Loading object IDs from: {self.aux_data_readers[0].get_reader_info()}")
+            primary_df = self.aux_data_readers[0].read_rows(self.block_start, block_size)
+            self.block_start += len(primary_df)
 
-        # Load and join in the other data frames.
-        for reader in self.object_readers:
+            obj_ids = primary_df["ObjID"].unique().tolist()
+            ephem_df = None
+
+        # No more data to load.
+        if len(obj_ids) == 0:
+            return None
+
+        # Load in the data for this block.
+        if ephem_df is None:
+            ephem_df = self.ephem_reader.read_objects(obj_ids)
+        ephem_ids = set(ephem_df["ObjID"].unique().tolist())
+
+        for reader in self.aux_data_readers:
             verboselog(f"Reading input file: {reader.get_reader_info()}")
-            current_df = reader.read_objects(objid_list)
+            current_df = reader.read_objects(obj_ids)
+        
+            # Check that the new dataframe has at least the object IDs matching
+            # the ephemeris frame.
+            current_ids = set(pd.unique(current_df["ObjID"]).astype(str))
+            if not ephem_ids.issubset(current_ids):
+                pplogger.error("ERROR: At least one missing ObjID in {reader.get_reader_info()}")
+                sys.exit("ERROR: At least one missing ObjID {reader.get_reader_info()}")
 
-            # Check that the new dataframe has the correct object IDs.
-            current_objs = pd.unique(current_df["ObjID"]).astype(str)
-            if set(current_objs) != set(objid_list):
-                pplogger.error("ERROR: ObjectIDs do not match primary table.")
-                sys.exit("ERROR: ObjectIDs do not match primary table.")
-
-            primary_df = primary_df.join(current_df.set_index("ObjID"), on="ObjID")
-
-        return primary_df
+            ephem_df = ephem_df.join(current_df.set_index("ObjID"), on="ObjID")
+        
+        return ephem_df
--- tests/readers/test_CombinedDataReader.py
+++ tests/readers/test_CombinedDataReader.py
@@ -13,8 +13,130 @@ from surveySimPP.utilities.dataUtilitiesForTests import get_test_filepath
 
 def test_CombinedDataReader():
     reader = CombinedDataReader()
-    reader.add_primary_reader(OrbitAuxReader(get_test_filepath("PPReadAllInput_orbits.des"), "whitespace"))
-    reader.add_reader(CSVDataReader(get_test_filepath("PPReadAllInput_params.txt"), "whitespace"))
-    # reader.add_reader(OIFDataReader(get_test_filepath("PPReadAllInput_oif.txt"), "csv"))
-    res_df = reader.read_block()
-    assert len(res_df) == 10
+    reader.add_ephem_reader(OIFDataReader(get_test_filepath("PPReadAllInput_oif.txt"), "csv"))
+    reader.add_aux_data_reader(OrbitAuxReader(get_test_filepath("PPReadAllInput_orbits.des"), "whitespace"))
+    reader.add_aux_data_reader(CSVDataReader(get_test_filepath("PPReadAllInput_params.txt"), "whitespace"))
+
+    # Read the first 5 objects from the orbits file. This corresponds to 4 objects and 668 rows
+    # in the ephemeris file.
+    res_df = reader.read_block(block_size=5)
+    assert len(res_df["ObjID"].unique().tolist()) == 4
+    assert len(res_df) == 334 
+
+    # Check that the first line matches.
+    expected_data = {
+        "ObjID": "632",
+        "FieldID": 38059,
+        "FieldMJD": 60277.351867,
+        "AstRange(km)": 983057302.988296,
+        "AstRangeRate(km/s)": -27.914,
+        "AstRA(deg)": 143.141481,
+        "AstRARate(deg/day)": 0.024483,
+        "AstDec(deg)": 8.677660,
+        "AstDecRate(deg/day)": -0.022025,
+        "Ast-Sun(J2000x)(km)": -718755527.053,
+        "Ast-Sun(J2000y)(km)": 707115399.940,
+        "Ast-Sun(J2000z)(km)": 202146766.832,
+        "Ast-Sun(J2000vx)(km/s)": -9.461,
+        "Ast-Sun(J2000vy)(km/s)": -9.435,
+        "Ast-Sun(J2000vz)(km/s)": -3.858,
+        "Obs-Sun(J2000x)(km)": 58803455.841,
+        "Obs-Sun(J2000y)(km)": 124187416.914,
+        "Obs-Sun(J2000z)(km)": 53827633.096,
+        "Obs-Sun(J2000vx)(km/s)": -28.129,
+        "Obs-Sun(J2000vy)(km/s)": 10.565,
+        "Obs-Sun(J2000vz)(km/s)": 4.677,
+        "Sun-Ast-Obs(deg)": 8.010336,
+        "t_0": 54800.0,
+        "t_p": 23466.22367,
+        "argperi": 284.5519,
+        "node": 217.91073,
+        "incl": 5.37133,
+        "e": 0.4966,
+        "q": 6.88417,
+        "H_r": 14.23,
+        "u-r": 1.72,
+        "g-r": 0.48,
+        "i-r": -0.11,
+        "z-r": -0.12,
+        "y-r": -0.12,
+        "GS": 0.15,
+    }
+    assert_equal(set(res_df.columns.values), set(expected_data.keys()))
+    for col in expected_data.keys():
+        assert expected_data[col] == res_df.iloc[0][col]
+
+    # Read the next 5 objects from the orbits file. This corresponds to 5 objects and 334 rows
+    # in the ephemeris file.
+    res_df = reader.read_block(block_size=5)
+    assert len(res_df["ObjID"].unique().tolist()) == 5
+    assert len(res_df) == 334
+
+    # Check that the id in the first row is '39262'
+    assert res_df.iloc[0]["ObjID"] == "39262"
+
+
+def test_CombinedDataReader_ephem():
+    """Read with ephemeris as the primary key."""
+    reader = CombinedDataReader()
+    reader.add_ephem_reader(OIFDataReader(get_test_filepath("PPReadAllInput_oif.txt"), "csv"))
+    reader.add_aux_data_reader(OrbitAuxReader(get_test_filepath("PPReadAllInput_orbits.des"), "whitespace"))
+    reader.add_aux_data_reader(CSVDataReader(get_test_filepath("PPReadAllInput_params.txt"), "whitespace"))
+
+    # Read the first 5 objects from the orbits file. This corresponds to 4 objects and 668 rows
+    # in the ephemeris file.
+    res_df = reader.read_block(block_size=200, ephem_primary=True)
+    assert len(res_df["ObjID"].unique().tolist()) == 4
+    assert len(res_df) == 334 
+
+    # Check that the first line matches.
+    expected_data = {
+        "ObjID": "632",
+        "FieldID": 38059,
+        "FieldMJD": 60277.351867,
+        "AstRange(km)": 983057302.988296,
+        "AstRangeRate(km/s)": -27.914,
+        "AstRA(deg)": 143.141481,
+        "AstRARate(deg/day)": 0.024483,
+        "AstDec(deg)": 8.677660,
+        "AstDecRate(deg/day)": -0.022025,
+        "Ast-Sun(J2000x)(km)": -718755527.053,
+        "Ast-Sun(J2000y)(km)": 707115399.940,
+        "Ast-Sun(J2000z)(km)": 202146766.832,
+        "Ast-Sun(J2000vx)(km/s)": -9.461,
+        "Ast-Sun(J2000vy)(km/s)": -9.435,
+        "Ast-Sun(J2000vz)(km/s)": -3.858,
+        "Obs-Sun(J2000x)(km)": 58803455.841,
+        "Obs-Sun(J2000y)(km)": 124187416.914,
+        "Obs-Sun(J2000z)(km)": 53827633.096,
+        "Obs-Sun(J2000vx)(km/s)": -28.129,
+        "Obs-Sun(J2000vy)(km/s)": 10.565,
+        "Obs-Sun(J2000vz)(km/s)": 4.677,
+        "Sun-Ast-Obs(deg)": 8.010336,
+        "t_0": 54800.0,
+        "t_p": 23466.22367,
+        "argperi": 284.5519,
+        "node": 217.91073,
+        "incl": 5.37133,
+        "e": 0.4966,
+        "q": 6.88417,
+        "H_r": 14.23,
+        "u-r": 1.72,
+        "g-r": 0.48,
+        "i-r": -0.11,
+        "z-r": -0.12,
+        "y-r": -0.12,
+        "GS": 0.15,
+    }
+    assert_equal(set(res_df.columns.values), set(expected_data.keys()))
+    for col in expected_data.keys():
+        assert expected_data[col] == res_df.iloc[0][col]
+
+    # Read the next 5 objects from the orbits file. This corresponds to 5 objects and 334 rows
+    # in the ephemeris file.
+    res_df = reader.read_block(block_size=5)
+    assert len(res_df["ObjID"].unique().tolist()) == 5
+    assert len(res_df) == 334
+
+    # Check that the id in the first row is '39262'
+    assert res_df.iloc[0]["ObjID"] == "39262"
