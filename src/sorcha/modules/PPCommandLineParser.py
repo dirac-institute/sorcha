0d6962c00657f18fa95d906ae3899d044cbc6223
Jeremy Kubica <104161096+jeremykubica@users.noreply.github.com>
2023-08-23 15:35:10 -0400

    Moves the logging earlier

--- src/sorcha/modules/PPCommandLineParser.py
+++ src/sorcha/modules/PPCommandLineParser.py
@@ -110,4 +110,9 @@ def PPCommandLineParser(args):
             )
         )
 
+    # If verbose logging is on, log all the command line settings to INFO.
+    if args.v:
+        for flag, value in cmd_args_dict.items():
+            pplogger.info(f"Using commandline setting {flag} = {value}")
+
     return cmd_args_dict
