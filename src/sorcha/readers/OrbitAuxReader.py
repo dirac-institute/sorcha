79e90baa29a164f0804c9a27600c9bb4b028e744
Max West <maxwest@uw.edu>
2023-08-24 16:20:54 -0700

    OrbitAuxReader changes

--- src/sorcha/readers/OrbitAuxReader.py
+++ src/sorcha/readers/OrbitAuxReader.py
@@ -58,59 +58,34 @@ class OrbitAuxReader(CSVDataReader):
         pplogger = logging.getLogger(__name__)
 
         if not "FORMAT" in input_table.columns:
-            pplogger.error(
-                "ERROR: PPReadOrbitFile: Orbit format must be provided."
-            )
-            sys.exit(
-                "ERROR: PPReadOrbitFile: Orbit format must be provided."
-            )
+            pplogger.error("ERROR: PPReadOrbitFile: Orbit format must be provided.")
+            sys.exit("ERROR: PPReadOrbitFile: Orbit format must be provided.")
 
         if len(input_table.columns) != 9:
-            pplogger.error(
-                "ERROR: Please only provide the required columns in your orbits file."
-            )
-            sys.exit(
-                "ERROR: Please only provide the required columns in your orbits file."
-            )
-
+            pplogger.error("ERROR: Please only provide the required columns in your orbits file.")
+            sys.exit("ERROR: Please only provide the required columns in your orbits file.")
 
         orb_format = input_table["FORMAT"].iloc[0]
         if not np.all(orb_format == input_table["FORMAT"].values):
-            pplogger.error(
-                "ERROR: Orbit file must have a consistent FORMAT (COM, KEP, or CART)."
-            )
-            sys.exit(
-                "ERROR: Orbit file must have a consistent FORMAT (COM, KEP, or CART)."
-            )
+            pplogger.error("ERROR: Orbit file must have a consistent FORMAT (COM, KEP, or CART).")
+            sys.exit("ERROR: Orbit file must have a consistent FORMAT (COM, KEP, or CART).")
 
-        keplerian_elements = ["ObjID", "a", "e", "inc","node", "peri", "ma", "epoch"]
+        keplerian_elements = ["ObjID", "a", "e", "inc", "node", "peri", "ma", "epoch"]
         cometary_elements = ["ObjID", "q", "e", "inc", "node", "argPeri", "t_p", "epoch"]
-        cartesian_elements = ["ObjID", "x", "y", "z", "xdot", "ydot", "vdot", "epoch"]
+        cartesian_elements = ["ObjID", "x", "y", "z", "xdot", "ydot", "zdot", "epoch"]
 
         if orb_format == "KEP":
             if not all(column in input_table.columns for column in keplerian_elements):
-                pplogger.error(
-                    "ERROR: PPReadOrbitFile: Must provide all keplerian orbital elements."
-                )
-                sys.exit(
-                    "ERROR: PPReadOrbitFile: Must provide all keplerian orbital elements."
-                )
+                pplogger.error("ERROR: PPReadOrbitFile: Must provide all keplerian orbital elements.")
+                sys.exit("ERROR: PPReadOrbitFile: Must provide all keplerian orbital elements.")
         elif orb_format == "COM":
             if not all(column in input_table.columns for column in cometary_elements):
-                pplogger.error(
-                    "ERROR: PPReadOrbitFile: Must provide all cometary orbital elements."
-                )
-                sys.exit(
-                    "ERROR: PPReadOrbitFile: Must provide all cometary orbital elements."
-                )
+                pplogger.error("ERROR: PPReadOrbitFile: Must provide all cometary orbital elements.")
+                sys.exit("ERROR: PPReadOrbitFile: Must provide all cometary orbital elements.")
         elif orb_format == "CART":
             if not all(column in input_table.columns for column in cartesian_elements):
-                pplogger.error(
-                    "ERROR: PPReadOrbitFile: Must provide all cartesian coordinate values."
-                )
-                sys.exit(
-                    "ERROR: PPReadOrbitFile: Must provide all cartesian coordinate values."
-                )
+                pplogger.error("ERROR: PPReadOrbitFile: Must provide all cartesian coordinate values.")
+                sys.exit("ERROR: PPReadOrbitFile: Must provide all cartesian coordinate values.")
         else:
             pplogger.error(
                 "ERROR: PPReadOrbitFile: Orbit format must be cometary (COM), keplerian (KEP), or cartesian (CART)."
