1aed598d1caa8064f38af7ba96f74f647269f5d1
Jeremy Kubica <104161096+jeremykubica@users.noreply.github.com>
2023-08-28 17:39:12 -0400

    Add an end to end diff test (#553)
    * Add an end to end diff test
    
    * Use `RandomState` for consistent random numbers across numpy versions.
    
    ---------
    
    Co-authored-by: drewoldag <47493171+drewoldag@users.noreply.github.com>

---
+++ tests/sorcha/test_demo_end2end.py
@@ -0,0 +1,27 @@
+import os
+import tempfile
+
+from sorcha.utilities.dataUtilitiesForTests import get_demo_filepath
+from sorcha.utilities.diffTestUtils import compare_result_files, override_seed_and_run
+
+
+def test_demo_end2end():
+    """run the full rubin sim to ensure there are no errors."""
+    golden_dir = get_demo_filepath("goldens")
+    golden_fn = os.path.join(golden_dir, "out_end2end.csv")
+    print(f"Golden File: {golden_fn}")
+    if not os.path.isdir(golden_dir) or not os.path.isfile(golden_fn):
+        print("ERROR: No goldens generated. You must first run:")
+        print("  python src/sorcha/utilities/generateGoldens.py")
+        assert False
+
+    with tempfile.TemporaryDirectory() as dir_name:
+        override_seed_and_run(dir_name)
+        res_file = os.path.join(dir_name, "out_end2end.csv")
+        assert os.path.isfile(res_file)
+        print(f"Res File: {res_file}")
+
+        if not compare_result_files(res_file, golden_fn):
+            print("Result files do not match. There may be an error in the code, or you may need")
+            print("to regenerate the goldens with 'python src/sorcha/utilities/generateGoldens.py'")
+            assert False
