f807860dbd1f6cbf614d4e479721d448af84ce15
Mario Juric <mjuric@astro.washington.edu>
2023-09-01 15:41:11 -0700

    Switch PPLinkingFilter to "mini-difi" (#566)
    * Switch PPLinkingFilter to "mini-difi" implementation
    
    This should speed up the execution at the expense of some code complexity.
    
    * mini-difi: Linting fixes

--- src/sorcha/modules/PPLinkingFilter.py
+++ src/sorcha/modules/PPLinkingFilter.py
@@ -3,7 +3,7 @@ import numpy as np
 from difi.metrics import NightlyLinkagesMetric
 
 
-def PPLinkingFilter(
+def PPLinkingFilter_OLD(
     observations,
     detection_efficiency,
     min_observations,
@@ -93,5 +93,88 @@ def PPLinkingFilter(
     )
 
     linked_object_observations = observations[observations["ObjID"].isin(findable["object_id"])]
+
+    return linked_object_observations
+
+
+def PPLinkingFilter(
+    observations,
+    detection_efficiency,
+    min_observations,
+    min_tracklets,
+    tracklet_interval,
+    minimum_separation,
+    maximum_time,
+    survey_name="lsst",
+):
+    """
+    A function which mimics the effects of the SSP linking process by looking
+    for valid tracklets within valid tracks and only outputting observations
+    which would be thus successfully "linked" by SSP.
+
+    Parameters:
+    -----------
+    detection_efficiency (float): the fractional percentage of successfully linked
+    detections.
+
+    min_observations (int): the minimum number of observations in a night required
+    to form a tracklet.
+
+    min_tracklets (int): the minimum number of tracklets required to form a valid track.
+
+    tracklet_interval (int): the time window (in days) in which the minimum number of
+    tracklets must occur to form a valid track.
+
+    minimum_separation (float): the minimum separation inside a tracklet for it
+    to be recognised as motion between images (in arcseconds).
+
+    maximum_time (float): # Maximum time separation (in days) between subsequent observations in a tracklet.
+
+    rng (numpy Generator object): numpy random number generator object.
+
+    survey_name (str): a string with the survey name. used for time-zone purposes.
+    Currently only accepts "lsst", "LSST".
+
+    Returns:
+    -----------
+    observations_out (pandas dataframe): a pandas dataframe containing observations
+    of linked objects only.
+
+    """
+
+    # create the ndarray that the linker expects
+    from sorcha.modules.miniDifi import linkObservations
+
+    obsv = pd.DataFrame(
+        {
+            "ssObjectId": observations["ObjID"],
+            "diaSourceId": observations["FieldID"],
+            "midPointTai": observations["FieldMJD"],
+            "ra": observations["AstRA(deg)"],
+            "decl": observations["AstDec(deg)"],
+        }
+    )
+    nameLen = obsv["ssObjectId"].str.len().max()
+    obsv = obsv.to_records(
+        index=False,
+        column_dtypes=dict(ssObjectId=f"a{nameLen}", diaSourceId="u8", midPointTai="f8", ra="f8", decl="f8"),
+    )
+
+    # link
+    obj = linkObservations(
+        obsv,
+        seed=0,
+        objectId="ssObjectId",
+        maxdt_minutes=maximum_time * 24 * 60,
+        minlen_arcsec=minimum_separation,
+        window=tracklet_interval,
+        nlink=min_tracklets,
+        p=detection_efficiency,
+    )
+
+    # unpack the results and filter the observations
+    objs_found = obj["ssObjectId"][~np.isnan(obj["discoverySubmissionDate"])]
+    obsv_found = np.isin(obsv["ssObjectId"], objs_found)
+    linked_object_observations = observations.iloc[obsv_found]
 
     return linked_object_observations
