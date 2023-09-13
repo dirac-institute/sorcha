a6b190225ac10a3f18eda8df31a3d8f4cb88aaaf
drewoldag <47493171+drewoldag@users.noreply.github.com>
2023-09-13 15:55:14 -0700

    Remove unused code from test.

--- demo/test_bench.py
+++ demo/test_bench.py
@@ -8,21 +8,6 @@ if __name__ == "__main__":  # pragma: no cover
     parser.add_argument("--object_type", default="mba", help="The type of objects to test (mba or tno).")
     args = parser.parse_args()
 
-    cmd_args_dict_old = {
-        "paramsinput": "./data_files/mpcorb_physical.csv",
-        "orbinfile": "./data_files/mpcorb_orbits.csv",
-        "oifoutput": "./data/out/ephemeris_output.csv",
-        "configfile": "./demo/test_bench_config.ini",
-        "pointing_database": "./demo/baseline_v2.0_1yr.db",
-        "outpath": "./data/out",
-        "makeTemporaryEphemerisDatabase": False,
-        "readTemporaryEphemerisDatabase": False,
-        "deleteTemporaryEphemerisDatabase": False,
-        "surveyname": "LSST",
-        "outfilestem": "out_mpcorb",
-        "verbose": False,
-    }
-
     cmd_args_dict = {
         "paramsinput": "./demo/mba_sample_1000_physical.csv",
         "orbinfile": "./demo/mba_sample_1000_orbit.csv",
