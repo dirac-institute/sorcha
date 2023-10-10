f2ba36d8a11e7f565059cab29bf02f3bba498533
Meg Schwamb <megan.schwamb@gmail.com>
2023-10-10 10:51:17 +0100

    Update __init__.py
    add in bits recommended by MJ to get the version functioning imported and working

--- src/sorcha/__init__.py
+++ src/sorcha/__init__.py
@@ -11,3 +11,10 @@ try:
     from sorcha_community_utils import *
 except ModuleNotFoundError:
     pass
+
+try:
+    from ._version import version as __version__
+    from ._version import version_tuple
+except ImportError:
+    __version__ = "unknown version"
+    version_tuple = (0, 0, "unknown version")
