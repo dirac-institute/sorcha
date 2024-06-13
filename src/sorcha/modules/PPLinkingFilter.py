dfc2e1d1c748fc0ebac3b5ccee47150ad6a3f631
Steph Merritt <s.merritt@qub.ac.uk>
2024-06-13 16:05:30 +0100

    Removing debug statements.

--- src/sorcha/modules/PPLinkingFilter.py
+++ src/sorcha/modules/PPLinkingFilter.py
@@ -85,14 +85,10 @@ def PPLinkingFilter(
     obsv_found = np.isin(obsv["ssObjectId"], objs_found)
     observations["object_linked"] = obsv_found
 
-    observations.to_csv("before_dropping_unlinked.csv")
-
     if drop_unlinked is None:
         linked_object_observations = observations.iloc[obsv_found]
     else:
         linked_object_observations = observations
-
-    linked_object_observations.to_csv("before_date.csv")
 
     # adding discovery submission date
     obj_discovery = pd.DataFrame(
@@ -100,7 +96,5 @@ def PPLinkingFilter(
     )
     obj_discovery["ObjID"] = obj_discovery["ObjID"].str.decode("utf-8")
     linked_object_observations = pd.merge(linked_object_observations, obj_discovery, on="ObjID")
-
-    linked_object_observations.to_csv("after_date.csv")
 
     return linked_object_observations
