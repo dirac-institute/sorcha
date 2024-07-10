59102a55164af11be752de115dac9f408b3904e2
Mario Juric <mjuric@astro.washington.edu>
2024-07-10 16:57:19 -0700

    quicktest.sh: update command line and tests

--- demo/quicktest.sh
+++ demo/quicktest.sh
@@ -9,18 +9,21 @@
 
 set -e
 
-trap '{ rm -f quicktest-result.csv; } ' EXIT
+trap '{ rm -f quicktest-result.*; } ' EXIT
 
 test -d demo || { echo "$0 must be run from the top-level source code directory"; exit -1; }
 
-rm -f quicktest-result.csv
+rm -f quicktest-result.*
 
 sorcha bootstrap
-sorcha run -c ./demo/sorcha_config_demo.ini -p ./demo/sspp_testset_colours.txt -ob ./demo/sspp_testset_orbits.des -pd ./demo/baseline_v2.0_1yr.db -o ./ -t quicktest-result
+sorcha run -c ./demo/sorcha_config_demo.ini -p ./demo/sspp_testset_colours.txt -ob ./demo/sspp_testset_orbits.des -pd ./demo/baseline_v2.0_1yr.db -o ./ -t quicktest-result -st quicktest-result.stats
 
 # check that the output file size is larger than ~10k
 # (NOTE: this is just a rough check, we should test against known output here!)
 filesize=$(wc -c quicktest-result.csv | awk '{print $1}')
 test $filesize -gt 10000
+
+# check that the stats file exists
+test -f quicktest-result.stats.csv
 
 echo "Quick test succesful!"
