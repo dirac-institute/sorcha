4f5d4551a54ff902c737b04fc7c52db36ca8d856
Mario Juric <mjuric@astro.washington.edu>
2024-07-11 16:59:33 -0700

    use the data files from the package directory
    Also use importlib.resources API to locate them (requires Python 3.9+)

--- demo/quicktest.sh
+++ demo/quicktest.sh
@@ -9,21 +9,27 @@
 
 set -e
 
-trap '{ rm -f quicktest-result.*; } ' EXIT
+trap "{ rm -rf \"$PWD/temp-demo-dir\"; }" EXIT
 
-test -d demo || { echo "$0 must be run from the top-level source code directory"; exit -1; }
-
-rm -f quicktest-result.*
+mkdir temp-demo-dir
+cd temp-demo-dir
 
 sorcha bootstrap
-sorcha run -c ./demo/sorcha_config_demo.ini -p ./demo/sspp_testset_colours.txt -ob ./demo/sspp_testset_orbits.des -pd ./demo/baseline_v2.0_1yr.db -o ./ -t quicktest-result -st quicktest-result.stats
+sorcha demo prepare
+
+# Let's get the most recent demo command line
+# (the `sed` part strips out ANSI escape sequences, see https://stackoverflow.com/a/43627833)
+sorcha_demo=$(sorcha demo howto | grep 'sorcha run' | sed "s,\x1B\[[0-9;]*[a-zA-Z],,g")
+echo "runnin demo command:"
+echo "  $sorcha_demo"
+$sorcha_demo
 
 # check that the output file size is larger than ~10k
 # (NOTE: this is just a rough check, we should test against known output here!)
-filesize=$(wc -c quicktest-result.csv | awk '{print $1}')
+filesize=$(wc -c testrun_e2e.csv | awk '{print $1}')
 test $filesize -gt 10000
 
 # check that the stats file exists
-test -f quicktest-result.stats.csv
+test -f testrun_stats.csv
 
 echo "Quick test succesful!"
