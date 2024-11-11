74a5e2547948c4cbf17383f8b92a768c05d35def
Ryan Lyttle <rlyttle09@qub.ac.uk>
2024-11-11 20:15:56 +0000

    Added dataclass and unit tests for simulation, filters, phasecurve and fadingfunction config sections.

--- src/sorcha/utilities/sorchaConfigs.py
+++ src/sorcha/utilities/sorchaConfigs.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass
 import configparser
 import logging
 import sys
-
+import numpy as np
 from sorcha.lightcurves.lightcurve_registration import LC_METHODS
 from sorcha.activity.activity_registration import CA_METHODS
 
@@ -11,9 +11,7 @@ from sorcha.activity.activity_registration import CA_METHODS
 class inputConfigs:
     """Data class for holding INPUTS section configuration file keys and validating them."""
 
-    ephemerides_type: str = (
-        ""  # make sure any defaults are "falsy" so we know when one hasn't populated properly
-    )
+    ephemerides_type: str = ("")
     """Simulation used for ephemeris input."""
 
     eph_format: str = ""
@@ -47,17 +45,177 @@ class inputConfigs:
         check_value_in_list(self.aux_format, ["comma", "whitespace", "csv"], "aux_format")
         self.size_serial_chunk = cast_as_int(self.size_serial_chunk, "size_serial_chunk")
 
+@dataclass
+class simulationConfigs:
+    """Data class for holding SIMULATION section configuration file keys and validating them"""
+    
+    ar_ang_fov: float = 0.0
+    """the field of view of our search field, in degrees"""
+
+    ar_fov_buffer: float = 0.0
+    """the buffer zone around the field of view we want to include, in degrees"""
+    
+    ar_picket: float = 0.0
+    """imprecise discretization of time that allows us to move progress our simulations forward without getting too granular when we don't have to. the unit is number of days."""
+
+    ar_obs_code: str = "" 
+    """the obscode is the MPC observatory code for the provided telescope."""
+
+    ar_healpix_order: int = 0
+    """the order of healpix which we will use for the healpy portions of the code."""
+
+    def __post_init__(self):
+        """Automagically validates the simulation configs after initialisation."""
+        self._validate_simulation_configs()
+    def _validate_simulation_configs(self):
+
+        # make sure all the mandatory keys have been populated.
+        check_key_exists(self.ar_ang_fov, "ar_ang_fov")
+        check_key_exists(self.ar_fov_buffer, "ar_fov_buffer")
+        check_key_exists(self.ar_picket, "ar_picket")
+        check_key_exists(self.ar_obs_code, "ar_obs_code")
+        check_key_exists(self.ar_healpix_order, "ar_healpix_order")
+
+        # some additional checks to make sure they all make sense!
+        self.ar_ang_fov = cast_as_float(self.ar_ang_fov,"ar_ang_fov")
+        self.ar_fov_buffer = cast_as_float(self.ar_fov_buffer,"ar_fov_buffer")
+        self.ar_picket = cast_as_int(self.ar_picket, "ar_picket")
+        self.ar_obs_code = cast_as_str(self.ar_obs_code,"ar_obs_code")
+        self.ar_healpix_order = cast_as_int(self.ar_healpix_order,"ar_healpix_order")
+
+     
+@dataclass
+class filtersConfigs:
+    """Data class for holding FILTERS section configuration file keys and validating them"""
+   
+    observing_filters: str = ""
+    """Filters of the observations you are interested in, comma-separated."""
+
+    survey_name : str =""
+    """survey name to be used for checking filters are correct"""
+
+    def __post_init__(self):
+        """Automatically validates the filters configs after initialisation."""
+        self._validate_filters_configs()
+        
+    def _validate_filters_configs(self):
+        
+        check_key_exists(self.observing_filters,"observing_filters")
+        self.observing_filters = cast_as_str(self.observing_filters,"obsering_filters")
+        check_key_exists(self.survey_name,"survey_name")
+        self.observing_filters= [e.strip() for e in self.observing_filters.split(",")]
+        self.check_for_correct_filters()
+
+    def check_for_correct_filters(self):
+
+        if self.survey_name in ["rubin_sim", "RUBIN_SIM","LSST","lsst"]:
+            lsst_filters = ["u", "g", "r", "i", "z", "y"]
+            filters_ok = all(elem in lsst_filters for elem in self.observing_filters)
+
+            if not filters_ok:
+                bad_list = np.setdiff1d(self.observing_filters, lsst_filters)
+                logging.error(
+                    "ERROR: Filter(s) {} given in config file are not recognised filters for {} survey.".format(
+                        bad_list, self.survey_name
+                    )
+                )
+                logging.error("Accepted {} filters: {}".format("LSST", lsst_filters))
+                logging.error("Change observing_filters in config file or select another survey.")
+                sys.exit(
+                    "ERROR: Filter(s) {} given in config file are not recognised filters for {} survey.".format(
+                        bad_list, self.survey_name
+                    )
+                )
+
+@dataclass
+class phasecurvesConfigs:
+    """Data class for holding PHASECURVES section configuration file keys and validating them"""
+    
+    phase_function: str = ""
+    """The phase function used to calculate apparent magnitude. The physical parameters input"""
+    
+    def __post_init__(self):
+        """Automatically validates the phasecurve configs after initialisation."""
+        self._validate_phasecurve_configs()
+    def _validate_phasecurve_configs(self):
+
+        # make sure all the mandatory keys have been populated.
+        check_key_exists(self.phase_function, "phase_function")
+
+        check_value_in_list(self.phase_function, ["HG","HG1G2","HG12","linear","none"], "phase_function")
+
+        
+@dataclass
+class fadingfunctionConfigs:
+    """Data class for holding FADINGFUNCTION section configuration file keys and validating them"""
+    
+    fading_function_on: bool = False
+    """Detection efficiency fading function on or off."""
+
+    fading_function_width: float = 0
+    """# Width parameter for fading function. Should be greater than zero and less than 0.5."""
+
+    fading_function_peak_efficiency: float = 0
+    """Peak efficiency for the fading function, called the 'fill factor' in Chelsey and Veres (2017)."""
+
+    def __post_init__(self):
+        """Automagically validates the fading function configs after initialisation."""
+        self._validate_fadingfunction_configs()
+    def _validate_fadingfunction_configs(self):
+
+        # make sure all the mandatory keys have been populated.
+        check_key_exists(self.fading_function_on, "fading_function_on")
+        self.fading_function_on = cast_as_bool(self.fading_function_on,"fading_function_on")
+        
+        if self.fading_function_on == True:
+
+            #when fading_function_on = true, fading_function_width and fading_function_peak_efficiency now mandatory
+            check_key_exists(self.fading_function_width, "fading_function_width")
+            check_key_exists(self.fading_function_peak_efficiency, "fading_function_peak_efficiency")
+            self.fading_function_width = cast_as_float(self.fading_function_width,"fading_function_width")
+            self.fading_function_peak_efficiency = cast_as_float(self.fading_function_peak_efficiency,"fading_function_efficiency")
+            
+            #boundaries conditions for both width and peak efficency
+            if self.fading_function_width <= 0.0 or self.fading_function_width > 0.5:
+                
+                logging.error(
+                "ERROR: fading_function_width out of bounds. Must be greater than zero and less than 0.5."
+            )
+                sys.exit(
+                    "ERROR: fading_function_width out of bounds. Must be greater than zero and less than 0.5."
+                )
+
+            if self.fading_function_peak_efficiency < 0.0 or self.fading_function_peak_efficiency > 1.0:
+                logging.error(
+                    "ERROR: fading_function_peak_efficiency out of bounds. Must be between 0 and 1."
+                    )
+                sys.exit(
+                    "ERROR: fading_function_peak_efficiency out of bounds. Must be between 0 and 1."
+                    )
+        
+        elif self.fading_function_on == False:
+            #making sure these aren't populated when self.fading_function_on = False
+            check_key_doesnt_exist(self.fading_function_width,"fading_function_width","but fading_function_on is False.")
+            check_key_doesnt_exist(self.fading_function_peak_efficiency,"fading_function_peak_efficiency","but fading_function_on is False.")
 
 @dataclass
 class sorchaConfigs:
     """Dataclass which stores configuration file keywords in dataclasses."""
 
-    inputs: inputConfigs = (
-        None  # setting empty defaults because we won't have these ready when we initialise the sorchaConfigs object
-    )
+    inputs: inputConfigs = None
     """inputConfigs dataclass which stores the keywords from the INPUT section of the config file."""
 
-    # simulation: simulationConfigs = None
+    simulation: simulationConfigs = None
+    """simulationConfigs dataclass which stores the keywords from the SIMULATION section of the config file."""
+
+    filters: filtersConfigs = None
+    """filtersConfigs dataclass which stores the keywords from the FILTERS section of the config file."""
+
+    phasecurve: phasecurvesConfigs = None
+    """phasecurveConfigs dataclass which stores the keywords from the PHASECURVES section of the config file."""
+
+    fadingfunction: fadingfunctionConfigs = None 
+    """fadingfunctionConfigs dataclass which stores the keywords from the FADINGFUNCTION section of the config file."""
 
     pplogger: None = None
     """The Python logger instance"""
@@ -80,17 +238,24 @@ class sorchaConfigs:
             )  # now we call a function that populates the class attributes
 
     def _read_configs_from_object(self, config_object):
+        """function that populates the class attributes"""
 
-        # do INPUTS section first
         inputs_dict = dict(
             config_object["INPUT"]
         )  # gets just the INPUTS section of the config file as a dictionary
         self.inputs = inputConfigs(**inputs_dict)
 
-        # SIMULATION would be next...
-        # simulation_dict = dict(config_object["SIMULATION"])
-        # self.simulation = simulationConfigs(**simulation_dict)
+        simulation_dict = dict(config_object["SIMULATION"])
+        self.simulation = simulationConfigs(**simulation_dict)
 
+        filter_dict = {**config_object["FILTERS"], "survey_name": self.survey_name} 
+        self.filters = filtersConfigs(**filter_dict)
+        
+        phasecurve_dict = dict(config_object["PHASECURVES"])
+        self.phasecurve = phasecurvesConfigs(**phasecurve_dict)
+
+        fadingfunction_dict = dict(config_object["FADINGFUNCTION"])
+        self.fadingfunction = fadingfunctionConfigs(**fadingfunction_dict)
 
 ## below are the utility functions used to help validate the keywords, add more as needed
 
@@ -108,6 +273,17 @@ def check_key_exists(value, key_name):
         )
 
 
+def check_key_doesnt_exist(value, key_name,reason):
+    #checks to make sure value doesn't exist 
+    if value:
+        logging.error(
+            f"ERROR: {key_name} supplied in config file {reason}"
+        )
+        sys.exit(
+            f"ERROR: {key_name} supplied in config file {reason}"
+        )
+
+
 def cast_as_int(value, key):
     # replaces PPGetIntOrExit: checks to make sure the value can be cast as an integer.
 
@@ -118,6 +294,39 @@ def cast_as_int(value, key):
         sys.exit(f"ERROR: expected an int for config parameter {key}. Check value in config file.")
 
     return int(value)
+
+def cast_as_str(value, key):
+   
+    try:
+        str(value)
+    except ValueError:
+        logging.error(f"ERROR: expected a str for config parameter {key}. Check value in config file.")
+        sys.exit(f"ERROR: expected a str for config parameter {key}. Check value in config file.")
+
+    return str(value)
+def cast_as_float(value, key):
+    # replaces PPGetFloatOrExit: checks to make sure the value can be cast as a float.
+
+    try:
+        float(value)
+    except ValueError:
+        logging.error(f"ERROR: expected a float for config parameter {key}. Check value in config file.")
+        sys.exit(f"ERROR: expected a float for config parameter {key}. Check value in config file.")
+
+    return float(value)
+
+def cast_as_bool(value, key):
+    # replaces PPGetBoolOrExit: checks to make sure the value can be cast as a bool.
+
+    str_value = str(value).strip()
+    
+    if str_value in ['true', '1', 'yes', 'y','True']:
+        return True
+    elif str_value in ['false', '0', 'no', 'n','False']:
+        return False
+    else:
+        logging.error(f"ERROR: expected a bool for config parameter {key}. Check value in config file.")
+        sys.exit(f"ERROR: expected a bool for config parameter {key}. Check value in config file.")
 
 
 def check_value_in_list(value, valuelist, key):
--- tests/sorcha/test_sorchaConfigs.py
+++ tests/sorcha/test_sorchaConfigs.py
@@ -1,7 +1,7 @@
 import pytest
 
 from sorcha.utilities.dataUtilitiesForTests import get_demo_filepath
-from sorcha.utilities.sorchaConfigs import sorchaConfigs, inputConfigs
+from sorcha.utilities.sorchaConfigs import sorchaConfigs, inputConfigs, simulationConfigs, filtersConfigs,phasecurvesConfigs ,fadingfunctionConfigs
 
 # these are the results we expect from sorcha_config_demo.ini
 correct_inputs = {
@@ -11,17 +11,46 @@ correct_inputs = {
     "aux_format": "whitespace",
     "pointing_sql_query": "SELECT observationId, observationStartMJD as observationStartMJD_TAI, visitTime, visitExposureTime, filter, seeingFwhmGeom as seeingFwhmGeom_arcsec, seeingFwhmEff as seeingFwhmEff_arcsec, fiveSigmaDepth as fieldFiveSigmaDepth_mag , fieldRA as fieldRA_deg, fieldDec as fieldDec_deg, rotSkyPos as fieldRotSkyPos_deg FROM observations order by observationId",
 }
+correct_simulation= {
+    "ar_ang_fov" : 2.06,
+    "ar_fov_buffer" : 0.2,
+    "ar_picket" : 1,
+    "ar_obs_code" : "X05",
+    "ar_healpix_order" : 6
+    }
+
+correct_filters= {
+    "observing_filters" : "r,g,i,z,u,y",
+    "survey_name" : "rubin_sim"
+
+}
+correct_filters_read= {
+    "observing_filters" : ['r','g','i','z','u','y'],
+    "survey_name" : "rubin_sim"
+
+}
+
+correct_phasecurve= {"phase_function": "HG"}
+
+correct_fadingfunction = {
+    "fading_function_on" : True,
+    "fading_function_width" : 0.1,
+    "fading_function_peak_efficiency" : 1.
+}
 
 
 def test_sorchaConfigs():
     # general test to make sure, overall, everything works. checks just one file: sorcha_config_demo.ini
 
     config_file_location = get_demo_filepath("sorcha_config_demo.ini")
-    test_configs = sorchaConfigs(config_file_location)
+    test_configs = sorchaConfigs(config_file_location,'rubin_sim')
 
     # check each section to make sure you get what you expect
     assert correct_inputs == test_configs.inputs.__dict__
-
+    assert correct_simulation == test_configs.simulation.__dict__
+    assert correct_filters_read == test_configs.filters.__dict__
+    assert correct_phasecurve == test_configs.phasecurve.__dict__
+    assert correct_fadingfunction == test_configs.fadingfunction.__dict__
 
 def test_inputConfigs():
 
@@ -32,7 +61,7 @@ def test_inputConfigs():
 
     # now we check some wrong inputs to make sure they're caught correctly. size_serial_chunk has to be castable as an integer
     input_configs["size_serial_chunk"] = "five thousand"
-
+    
     # we're telling pytest that we expect this command to fail with a SystemExit
     with pytest.raises(SystemExit) as error_text:
         test_configs = inputConfigs(**input_configs)
@@ -42,7 +71,6 @@ def test_inputConfigs():
         error_text.value.code
         == "ERROR: expected an int for config parameter size_serial_chunk. Check value in config file."
     )
-
 
 @pytest.mark.parametrize(
     "key_name", ["ephemerides_type", "eph_format", "size_serial_chunk", "aux_format", "pointing_sql_query"]
@@ -85,3 +113,201 @@ def test_inputConfigs_inlist(key_name, expected_list):
         error_text.value.code
         == f"ERROR: value definitely_fake_bad_key for config parameter {key_name} not recognised. Expecting one of: {expected_list}."
     )
+
+
+def test_filtersConfigs():
+
+    filters_configs=correct_filters.copy()
+
+    test_configs= filtersConfigs(**filters_configs)
+    assert test_configs.__dict__ == correct_filters_read
+    
+    filters_configs["observing_filters"]='a,f,u,g'
+    with pytest.raises(SystemExit) as error_text:
+        test_configs = filtersConfigs(**filters_configs)
+    assert (error_text.value.code == "ERROR: Filter(s) ['a' 'f'] given in config file are not recognised filters for rubin_sim survey.")
+
+@pytest.mark.parametrize(
+    "key_name", ["observing_filters","survey_name"] )
+def test_filtersConfigs_mandatory(key_name):
+    # this loops through the mandatory keys and makes sure the code fails correctly when each is missing
+
+    filter_configs = correct_filters.copy()
+    del filter_configs[key_name]
+
+    with pytest.raises(SystemExit) as error_text:
+        test_configs = filtersConfigs(**filter_configs)
+
+    assert (
+        error_text.value.code
+        == f"ERROR: No value found for required key {key_name} in config file. Please check the file and try again."
+    )
+
+def test_simulationConfigs():
+
+    simulation_configs=correct_simulation.copy()
+    test_configs= simulationConfigs(**simulation_configs)
+    assert test_configs.__dict__ == simulation_configs
+    
+    simulation_configs["ar_picket"]="one"
+
+    with pytest.raises(SystemExit) as error_text:
+        test_configs = simulationConfigs(**simulation_configs)
+    
+    assert (error_text.value.code == "ERROR: expected an int for config parameter ar_picket. Check value in config file.")
+
+@pytest.mark.parametrize(
+    "key_name", ["ar_ang_fov" , "ar_fov_buffer", "ar_picket", "ar_obs_code", "ar_healpix_order"])
+def test_simulationConfigs_mandatory(key_name):
+    # this loops through the mandatory keys and makes sure the code fails correctly when each is missing
+
+    simulation_configs = correct_simulation.copy()
+
+    del simulation_configs[key_name]
+
+    with pytest.raises(SystemExit) as error_text:
+        test_configs = simulationConfigs(**simulation_configs)
+
+    assert (
+        error_text.value.code
+        == f"ERROR: No value found for required key {key_name} in config file. Please check the file and try again."
+    )
+
+
+
+def test_phasecurevConfigs():
+
+    phasecurve_configs=correct_phasecurve.copy()
+    test_configs= phasecurvesConfigs(**phasecurve_configs)
+    assert test_configs.__dict__ == phasecurve_configs
+    
+    phasecurve_configs["phase_function"]=10
+
+    with pytest.raises(SystemExit) as error_text:
+        test_configs = phasecurvesConfigs(**phasecurve_configs)
+    
+    assert (error_text.value.code == "ERROR: value 10 for config parameter phase_function not recognised. Expecting one of: ['HG', 'HG1G2', 'HG12', 'linear', 'none'].")
+
+
+
+
+@pytest.mark.parametrize(
+    "key_name", ["phase_function"] )
+def test_phasecurveConfigs_mandatory(key_name):
+    # this loops through the mandatory keys and makes sure the code fails correctly when each is missing
+
+    phasecurves_configs = correct_phasecurve.copy()
+
+    del phasecurves_configs[key_name]
+
+    with pytest.raises(SystemExit) as error_text:
+        test_configs = phasecurvesConfigs(**phasecurves_configs)
+
+    assert (
+        error_text.value.code
+        == f"ERROR: No value found for required key {key_name} in config file. Please check the file and try again."
+    )
+
+@pytest.mark.parametrize(
+    "key_name, expected_list",
+    [
+        ("phase_function", "['HG', 'HG1G2','HG12','linear','none']")
+    ],
+)
+def test_phasecurveConfigs_inlist(key_name, expected_list):
+    # this loops through the inputs keys that need to have one of several set values and makes sure the correct error message triggers when they're not
+
+    phasecurve_configs = correct_phasecurve.copy()
+
+    phasecurve_configs[key_name] = "definitely_fake_bad_key"
+
+    with pytest.raises(SystemExit) as error_text:
+        test_configs = phasecurvesConfigs(**phasecurve_configs)
+
+    assert (
+        error_text.value.code
+        == f"ERROR: value definitely_fake_bad_key for config parameter {key_name} not recognised. Expecting one of: {expected_list}."
+    )
+
+def test_fadingfunctionConfig():
+
+    fadingfunction_configs=correct_fadingfunction.copy()
+    test_configs= fadingfunctionConfigs(**fadingfunction_configs)
+    assert test_configs.__dict__ == fadingfunction_configs
+    
+    fadingfunction_configs["fading_function_width"] = 'ten'
+    with pytest.raises(SystemExit) as error_text:
+        test_configs = fadingfunctionConfigs(**fadingfunction_configs)
+    
+    assert (error_text.value.code == "ERROR: expected a float for config parameter fading_function_width. Check value in config file.")
+
+    fadingfunction_configs=correct_fadingfunction.copy()
+    test_configs= fadingfunctionConfigs(**fadingfunction_configs)
+    assert test_configs.__dict__ == fadingfunction_configs
+    
+    fadingfunction_configs["fading_function_on"] = 'ten'
+    with pytest.raises(SystemExit) as error_text:
+        test_configs = fadingfunctionConfigs(**fadingfunction_configs)
+    
+    assert (error_text.value.code == "ERROR: expected a bool for config parameter fading_function_on. Check value in config file.")
+
+    fadingfunction_configs=correct_fadingfunction.copy()
+    test_configs= fadingfunctionConfigs(**fadingfunction_configs)
+    assert test_configs.__dict__ == fadingfunction_configs
+    #testing it will correctly reject fading_function_width and fading_function_peak_efficiency
+    fadingfunction_configs["fading_function_on"] = "False"
+    with pytest.raises(SystemExit) as error_text:
+        test_configs = fadingfunctionConfigs(**fadingfunction_configs)
+    assert (error_text.value.code == "ERROR: fading_function_width supplied in config file but fading_function_on is False.")
+
+@pytest.mark.parametrize(
+    "key_name", ["fading_function_on","fading_function_width","fading_function_peak_efficiency"] )
+def test_fadingfunction_mandatory(key_name):
+    # this loops through the mandatory keys and makes sure the code fails correctly when each is missing
+
+    fadingfunction_configs = correct_fadingfunction.copy()
+
+    del fadingfunction_configs[key_name]
+
+    with pytest.raises(SystemExit) as error_text:
+        test_configs = fadingfunctionConfigs(**fadingfunction_configs)
+
+    assert (
+        error_text.value.code
+        == f"ERROR: No value found for required key {key_name} in config file. Please check the file and try again."
+    )
+
+@pytest.mark.parametrize(
+    "key_name", ["fading_function_width","fading_function_peak_efficiency"] )
+def test_fadingfunction_notrequired(key_name):
+# tests that "fading_function_width" and "fading_function_peak_efficiency" are not called when "fading_function_on" is false
+    fadingfunction_configs = correct_fadingfunction.copy()
+    fadingfunction_configs["fading_function_on"]= "False"
+    fadingfunction_configs["fading_function_width"] = 0
+    fadingfunction_configs["fading_function_peak_efficiency"] = 0
+    fadingfunction_configs[key_name]=0.5
+    with pytest.raises(SystemExit) as error_text:
+        test_configs = fadingfunctionConfigs(**fadingfunction_configs)
+    assert (
+        error_text.value.code
+        == f"ERROR: {key_name} supplied in config file but fading_function_on is False."
+    )
+
+@pytest.mark.parametrize(
+    "key_name", ["fading_function_width","fading_function_peak_efficiency"] )
+def test_fadingfunction_outofbounds(key_name):
+    fadingfunction_configs = correct_fadingfunction.copy()
+    fadingfunction_configs[key_name]=10
+    if key_name =="fading_function_width":
+        with pytest.raises(SystemExit) as error_text:
+            test_configs = fadingfunctionConfigs(**fadingfunction_configs)
+        assert (
+        error_text.value.code
+        == "ERROR: fading_function_width out of bounds. Must be greater than zero and less than 0.5."
+    )
+    if key_name == "fading_function_peak_efficiency":
+        with pytest.raises(SystemExit) as error_text:
+            test_configs = fadingfunctionConfigs(**fadingfunction_configs)
+        assert (
+        error_text.value.code
+        == "ERROR: fading_function_peak_efficiency out of bounds. Must be between 0 and 1.")
