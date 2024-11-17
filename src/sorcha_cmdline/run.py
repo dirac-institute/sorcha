756b1671932cda5b1c95bbc55edca993c48b7d3d
Meg Schwamb <megan.schwamb@gmail.com>
2024-11-17 12:19:11 +0000

    extra error message if no command line arguments
    Fixes #1019
    
    Extra error message if no command line arguments

--- src/sorcha_cmdline/run.py
+++ src/sorcha_cmdline/run.py
@@ -2,12 +2,14 @@
 # The `sorcha run` subcommand implementation
 #
 import argparse
+import sys
 
 
 def main():
     parser = argparse.ArgumentParser(
         formatter_class=argparse.ArgumentDefaultsHelpFormatter, description="Run a simulation."
     )
+
     required = parser.add_argument_group("Required arguments")
     required.add_argument(
         "-c",
@@ -115,6 +117,16 @@ def main():
         dest="st",
         default=None,
     )
+
+    # outputting an extra error message to the prompt if no arguments are included.
+    # we found from beta testing that a user unfamilar with linux/unix may not
+    # know the -h flag is what they need to get help
+
+    if len(sys.argv) == 1:
+        print(
+            "\n\nError: No command line arguments provided!!! \nUse the -h flag to see descriptions of the required and optional arguments needed to run Sorcha \n",
+            file=sys.stderr,
+        )
 
     args = parser.parse_args()
 
