31c70cd960427709a28c097b6ec308522cd29d69
Meg Schwamb <megan.schwamb@gmail.com>
2024-11-27 16:55:27 +0000

    fix typo

--- src/sorcha_cmdline/bootstrap.py
+++ src/sorcha_cmdline/bootstrap.py
@@ -8,7 +8,7 @@ from sorcha_cmdline.sorchaargparse import SorchaArgparse
 
 def main():  # pragma: no cover
     # parse the input arguments
-    parser = SorchaArgparse(argparse.ArgumentParser(prog="sorcha bootstrap"
+    parser = SorchaArgparse(argparse.ArgumentParser(prog="sorcha bootstrap", 
         description="Fetch the NAIF high precision EOP kernel file store its checksum."
     ))
    
