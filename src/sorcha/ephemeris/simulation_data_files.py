dd4dde20c0b04f9f58409a6649ff3f479a4cce90
Ryan Lyttle <rlyttle09@qub.ac.uk>
2024-12-03 21:32:18 +0000

    changed make_retriever function to work with dataclasses

--- src/sorcha/ephemeris/simulation_data_files.py
+++ src/sorcha/ephemeris/simulation_data_files.py
@@ -79,7 +79,6 @@ def make_retriever(directory_path: str = None, registry: dict = REGISTRY) -> poo
     registry : dictionary, optional
         A dictionary of file names to SHA hashes. Generally we'll not use SHA=None
         because the files we're tracking change frequently. Default = REGISTRY
-
     Returns
     -------
     : pooch
@@ -94,5 +93,34 @@ def make_retriever(directory_path: str = None, registry: dict = REGISTRY) -> poo
         base_url="",
         urls=URLS,
         registry=registry,
+        retry_if_failed=25,
+    )
+
+def make_retriever_notusedyet(sconfigs, directory_path: str = None) -> pooch.Pooch:
+    """Helper function that will create a Pooch object to track and retrieve files.
+
+    Parameters
+    ----------
+    directory_path : string, optional
+        The base directory to place all downloaded files. Default = None
+    registry : dictionary, optional
+        A dictionary of file names to SHA hashes. Generally we'll not use SHA=None
+        because the files we're tracking change frequently. Default = REGISTRY
+    sconfigs: dataclass
+        Dataclass of configuration file arguments.
+    Returns
+    -------
+    : pooch
+        The instance of a Pooch object used to track and retrieve files.
+    """
+    dir_path = pooch.os_cache("sorcha")
+    if directory_path:
+        dir_path = directory_path
+
+    return pooch.create(
+        path=dir_path,
+        base_url="",
+        urls=sconfigs.auxiliary.urls,
+        registry=sconfigs.auxiliary.registry,
         retry_if_failed=25,
     )
