b9597155c28fa8a1f0acfc875f52ecf8af9cfe73
Ryan Lyttle <rlyttle09@qub.ac.uk>
2026-07-17 14:09:41 +0100

    updated unit test for _split_kernel_path_str

--- tests/ephemeris/test_kernel_str_length.py
+++ tests/ephemeris/test_kernel_str_length.py
@@ -6,16 +6,16 @@ def test_kernel_str_length():
 
     # Each list in the tuple is a test containing the input, expected output and split value for the function, respectively
     test_strings = (
-        ["this is a test string", "this is a test +' 'string", 15],
+        ["this is a test string", "this is a test +',\n'string", 15],
         [
             "this is another test string which is slightly larger",
-            "this is another+' ' test string wh+' 'ich is slightly+' ' larger",
+            "this is another+',\n' test string wh+',\n'ich is slightly+',\n' larger",
             15,
         ],
         ["small string", "small string", 79],
         [
             "split will be the length of this string to see what happens",
-            "split will be the length of this string to see what happens+' '",
+            "split will be the length of this string to see what happens+',\n'",
             59,
         ],
     )
