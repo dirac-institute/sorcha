983c783cde9d37ebf3fe68751e0127630480a0c3
Pedro Bernardinelli <pedro.h.bernardinelli@gmail.com>
2026-09-12 09:20:19 -0300

    Update PPDetectionProbability.py

--- src/sorcha/modules/PPDetectionProbability.py
+++ src/sorcha/modules/PPDetectionProbability.py
@@ -101,5 +101,6 @@ def PPDetectionProbability(
         Probability of detection.
 
     """
-
+    if "limMag_perChip" in eph_df.columns:
+        limiting_magnitude_name = "limMag_perChip"
     return calcDetectionProbability(eph_df[magnitude_name], eph_df[limiting_magnitude_name], fillFactor, w)
