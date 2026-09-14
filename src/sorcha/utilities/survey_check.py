122a2a133194a76be9f1970d4d9a4908f94beffe
Ryan Lyttle <rlyttle09@qub.ac.uk>
2026-09-14 10:08:12 +0100

    changing function to do surveys

--- src/sorcha/utilities/survey_check.py
+++ src/sorcha/utilities/survey_check.py
@@ -1,4 +1,12 @@
-from sorcha.utilities.survey_constants import DICT_SURVEY_NAMES
+# dict of surveys for a given option, These are currently used in fov,  filters , expert  configs
+# Where the type of survey matters for running Sorcha (i.e. camera footprint, filters and certain features turned off)
+
+DICT_SURVEY_NAMES = {
+    "rubin": ["rubin_sim", "lsst"],  # rubin is used for any overall rubin process.
+    "rubin_sim": ["rubin_sim"],  # for rubin_sim funcitons
+    "lsst": ["lsst"],  # for lsst function
+    "des": ["des"],  # for des fucntions.
+}  # clean up comments and explain better detail.
 
 
 def is_survey_valid(survey_name, expected_survey):
